941fd04746c9530ad4cd8788e01a4d49b19f0e36
Japo <48571612+jhgakiyama@users.noreply.github.com>
2025-11-10 23:45:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function tick(){
 tick();
 setInterval(tick,1000);
 
-// Swiper init (unchanged)
+// Swiper init
 const swiper = new Swiper('.mySwiper',{
   loop: true,
   autoplay: { delay: 4500, disableOnInteraction: false },
